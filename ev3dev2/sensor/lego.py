b134d4f89b425c8a4855dc791e6525bb2ff59a9b
Lukasz Machura <lukasz.machura@gmail.com>
2020-10-18 22:29:36 +0200

    touch sensor

--- ev3dev2/sensor/lego.py
+++ ev3dev2/sensor/lego.py
@@ -7,6 +7,107 @@ from ev3dev2 import *
 if sys.version_info < (3, 4):
     raise SystemError('Must be using Python 3.4 or higher')
 
+#SensorTouch
+class TouchSensor(Sensor):
+    """
+    Touch Sensor
+    """
+
+    SYSTEM_CLASS_NAME = Sensor.SYSTEM_CLASS_NAME
+    SYSTEM_DEVICE_NAME_CONVENTION = Sensor.SYSTEM_DEVICE_NAME_CONVENTION
+
+    #: Button state
+    MODE_TOUCH = 'TOUCH'
+    MODES = (MODE_TOUCH, )
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+        if "clientID" in self.kwargs:  # local preference
+            self.clientID = self.kwargs['clientID']
+        elif "clientID" in globals():  # global question
+            if VERBOSE:
+                print("global ID")
+            self.clientID = clientID
+            self.kwargs['clientID'] = clientID
+        else:
+            self.kwargs['clientID'] = connection(dummy=not True)
+
+        self.connected = True
+        self.mode = TouchSensor.MODE_TOUCH
+
+    @property
+    def is_pressed(self):
+        """
+        A boolean indicating whether the current touch sensor is being
+        pressed.
+        """
+        self._ensure_mode(self.MODE_TOUCH)
+
+        out = vrep.simxCallScriptFunction(
+            clientID,
+            'Funciones',
+            vrep.sim_scripttype_childscript,
+            'SensorTouch',
+            [], [], [], bytearray(),
+            vrep.simx_opmode_oneshot_wait
+        )
+        returnCode, outInts, outFloats, outStrings, outBuffer = out
+
+        pressed = outInts[0]
+        print(pressed)
+        return bool(pressed)
+
+    @property
+    def is_released(self):
+        return not self.is_pressed
+
+    def _wait(self, wait_for_press, timeout_ms, sleep_ms):
+        tic = time.time()
+
+        if sleep_ms:
+            sleep_ms = float(sleep_ms / 1000)
+
+        # The kernel does not supoort POLLPRI or POLLIN for sensors so we have
+        # to drop into a loop and check often
+        while True:
+
+            if self.is_pressed == wait_for_press:
+                return True
+
+            if timeout_ms is not None and time.time() >= tic + timeout_ms / 1000:
+                return False
+
+            if sleep_ms:
+                time.sleep(sleep_ms)
+
+    def wait_for_pressed(self, timeout_ms=None, sleep_ms=10):
+        """
+        Wait for the touch sensor to be pressed down.
+        """
+        return self._wait(True, timeout_ms, sleep_ms)
+
+    def wait_for_released(self, timeout_ms=None, sleep_ms=10):
+        """
+        Wait for the touch sensor to be released.
+        """
+        return self._wait(False, timeout_ms, sleep_ms)
+
+    def wait_for_bump(self, timeout_ms=None, sleep_ms=10):
+        """
+        Wait for the touch sensor to be pressed down and then released.
+        Both actions must happen within timeout_ms.
+        """
+        start_time = time.time()
+
+        if self.wait_for_pressed(timeout_ms, sleep_ms):
+            if timeout_ms is not None:
+                timeout_ms -= int((time.time() - start_time) * 1000)
+            return self.wait_for_released(timeout_ms, sleep_ms)
+
+        return False
+
+
 class GyroSensor(Sensor):
     """
     LEGO EV3 gyro sensor.
---
+++ probot_touch_sensor.py
@@ -0,0 +1,39 @@
+# Create a program to make the robot move straight and then turn 90 degrees.
+# Import of necessary libraries
+from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor.lego import GyroSensor, TouchSensor
+from ev3dev2.display import Display
+from ev3dev2.led import Leds
+
+####
+# load probot_detection.ttt scene
+#
+
+# Connect two large motors to port B and port C
+tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
+gy = GyroSensor()
+ts = TouchSensor()
+d = Display()
+led = Leds()
+
+
+start_angle = gy.value()
+angle = start_angle
+
+led.set_color('BOTH', (0, 0, 1))
+while abs(angle - start_angle) < 180 :
+    angle = gy.value()
+    # you can see the results on the screen
+    print(angle, abs(angle - start_angle))
+    tank_pair.on(-10, 10)
+
+led.set_color('BOTH', 'GREEN')
+while not ts.is_pressed:
+    angle = gy.value()
+    # you can see the results on the screen
+    print(angle, abs(angle - start_angle), ts.is_pressed)
+    d.text_grid("Touch Sensor pressed: " + str(ts.is_pressed))
+    tank_pair.on(-20, -20)
+
+led.set_color('BOTH', 'RED')
+tank_pair.off()
