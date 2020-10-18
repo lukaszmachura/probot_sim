4547324458ca8215df84a18f5aaa192909985dc8
Lukasz Machura <lukasz.machura@gmail.com>
2020-10-19 00:55:54 +0200

    dict

--- probot_button.py
+++ probot_button.py
@@ -3,6 +3,15 @@ from ev3dev2.led import Leds
 from ev3dev2.display import Display
 from time import sleep, time
 
+buttons = {
+    'up': 5,
+    'down': 1,
+    'left': 3,
+    'right': 2,
+    'enter': 4,
+    'backspace': 6
+    }
+
 button = Button()
 leds = Leds()
 display = Display()
@@ -15,9 +24,10 @@ while time() - t < 1:
     display.text_grid(f"Button pressed: {button.any()}, Button number: {button.get_pressed_button_number}")
     print(button.any(), button.get_pressed_button_number)
 
-
+# works with numbers
 if button.wait_for_pressed(6):
     print("6 pressed")
 
+# and names
 if button.wait_for_pressed('up'):
     print('up pressed')
